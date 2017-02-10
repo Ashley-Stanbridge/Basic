93237ffef1e4065e1ffdcda044fd2a209d775a47
Ashley <Ashley@Ashleys-MBP.telecom>
2017-02-10 14:40:01 +1300

    Added routes

--- server.js
+++ server.js
@@ -9,4 +9,18 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/resume', function(req, res) {
+    res.sendFile(path.join(__dirname + '/resume.html'));
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+app.get('/contact', function(req, res) {
+    res.sendFile(path.join(__dirname + '/contact.html'));
+});
+
+
+
 app.listen(8080);
